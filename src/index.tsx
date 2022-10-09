6a1589d36bf4aa21fc0d61eeb638af56b1698d14
kftlfd <kftlfd@ex.ua>
2022-10-09 18:42:07 +0300

    add typescript

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import store from "./store";
 import ThemeProvider from "./components/ThemeContext";
 import App from "./components/App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   // <React.StrictMode>
   <StoreProvider store={store}>
