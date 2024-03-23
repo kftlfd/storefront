aec6e3e5c8e0c848d7e687574a1ae458ee0e82db
kftlfd <kftlfd@ex.ua>
2024-03-23 16:12:32 +0200

    add GraphQL

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -1,3 +1,4 @@
 export default {
   apiUrl: import.meta.env.VITE_API_URL,
+  gqlURL: import.meta.env.VITE_GQL_URL,
 };
--- frontend/src/types/vite-env.d.ts
+++ frontend/src/types/vite-env.d.ts
@@ -3,6 +3,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_API_URL: string;
+  readonly VITE_GQL_URL: string;
 }
 
 interface ImportMeta {
